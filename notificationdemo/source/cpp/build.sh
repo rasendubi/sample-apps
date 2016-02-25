46e28bf93a130d673b0bb21015f3f127d691941b
RostakaGmfun <rostawesomegd@gmail.com>
2016-02-25 17:12:43 +0200

    APP-53: Add expect-based test for Notification Demo

--- notificationdemo/source/cpp/build.sh
+++ notificationdemo/source/cpp/build.sh
@@ -19,7 +19,7 @@
 RUN_DIR=`pwd`
 
 function help {
-    echo "Choose one of the following: {build|run|deploy|clean}"
+    echo "Choose one of the following: {build|run|deploy|test|clean}"
     echo "Supported platforms: x86-64, edison"
     exit 1
 }
@@ -108,6 +108,17 @@ function run {
     ./$APP_NAME
 }
 
+function run_test {
+    rm $PROJECT_HOME/$BUILD_DIR/kaa.status
+    $PROJECT_HOME/test/test.exp $PROJECT_HOME/$BUILD_DIR/$APP_NAME 2> /dev/null
+    if [ $? -ne 0 ] ; then
+        echo "TEST FAILED"
+        exit 1
+    else
+        echo "TEST PASSED"
+    fi
+}
+
 for cmd in $@
 do
 
@@ -129,6 +140,12 @@ case "$cmd" in
         build_app
         run
         ;;
+    
+    test)
+        build_app
+        run_test
+        ;;
+
 
     clean)
         clean
